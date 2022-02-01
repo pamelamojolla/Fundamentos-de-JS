dd69b2f6c5bed57911a08d8ddd96ed885f588669
Pamela Mojolla <pamela.mojolla@hotmail.com>
2022-02-01 20:13:29 -0300

    função

---
+++ funcaoEmQuaseTudo.js
@@ -0,0 +1,6 @@
+console.log(typeof Object)//objeto é uma função
+
+class Produto {}
+console.log(typeof Produto)//uma classe é uma função
+
+//tudo é função dentro do JS.
---
+++ null_undefined.js
@@ -0,0 +1,8 @@
+let valor // variavel não inicializada no console é exibido undefined
+console.log(valor)
+
+valor = null //ausencia de valor ela não tem nenhum endereço na memoria para apontar
+console.log(valor)
+
+//sempre usar null quando quiser zerar o valor de uma variavel 
+//sempre deixar a linguagem usar undefined para algo ainda não definido ou inicializado
