2c780861dd01ce0b5b74eb50a5126894f32cb5cd
Pamela Mojolla <pamela.mojolla@hotmail.com>
2022-02-02 20:36:23 -0300

    usandoLet_Var_emLoop

---
+++ usarLetEmLoop.js
@@ -0,0 +1,5 @@
+for (let i = 0; i < 10; i++){
+    console.log(i);
+}
+
+console.log(i) // fora do bloco a variavel com let não fica disponivel 
---
+++ usarVarEmLoop.js
@@ -0,0 +1,7 @@
+//usando var em loop
+
+for (var i = 0; i < 10; i++){    //usando var em laço de repetição coloca a condição quando i tiver o valor de 10 ele sai do laço
+    console.log(i);
+}
+
+console.log('i =', i) //imprimindo a var i depois do laço ele estará disponivel 
