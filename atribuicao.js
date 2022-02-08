bbe34d7e089fb4a051b3e77124452a75a500181a
Pamela Mojolla <pamela.mojolla@hotmail.com>
2022-02-07 21:21:08 -0300

    Atribuição

---
+++ atribuicao.js
@@ -0,0 +1,19 @@
+//operadores de atribuição
+
+const a = 7 // sinal de = dentro do JS ele atribui um valor, ele está recebendo uma informação
+let b = 3
+
+b += a  // atalhos de atribuição nesse caso atribuiçao aditiva b = b + a
+console.log(b)
+
+b -= 4 // b = b - 4
+console.log(b)
+
+b *= 2 // b = b * 2
+console.log(b)
+
+b /= 2 // b = b / 2
+console.log(b)
+
+b %=2 // b = b % 2
+console.log(b)
